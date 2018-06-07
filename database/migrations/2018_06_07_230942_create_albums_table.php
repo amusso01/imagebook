c1de469ee1dd623ff70d1c673f73593aad5eb9c2
amuso01 <eandram@hotmail.it>
2018-06-08 00:58:41 +0100

    migration and controller

---
+++ database/migrations/2018_06_07_230942_create_albums_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('album_name');
+            $table->text('description');
+            $table->string('cover_image');
+            $table->timestamps();
+        });
+
+        //add constraint
+        Schema::table('albums',function($table){    
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
