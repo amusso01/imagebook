3b2c78646df686b7271b70efe85d2ab106cf858a
amuso01 <eandram@hotmail.it>
2018-06-09 01:29:55 +0100

    create album-intervention

--- app/image.php
+++ app/image.php
@@ -4,13 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class image extends Model
+class Image extends Model
 {
     protected $table = 'images';
   
     protected $fillable = array('album_id','description','image_name');
 
     public function album(){
-        return $this->belongsTo('App/album');
+        return $this->belongsTo('App\Album');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,66 +10,21 @@
 
 <div class="row text-center text-lg-left">
 
-  <div class="col-lg-3 col-md-4 col-xs-6">
-    <a href="#" class="d-block mb-4 h-100">
-      <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-    </a>
-  </div>
-  <div class="col-lg-3 col-md-4 col-xs-6">
-    <a href="#" class="d-block mb-4 h-100">
-      <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-    </a>
-  </div>
-  <div class="col-lg-3 col-md-4 col-xs-6">
-    <a href="#" class="d-block mb-4 h-100">
-      <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-    </a>
-  </div>
-  <div class="col-lg-3 col-md-4 col-xs-6">
-    <a href="#" class="d-block mb-4 h-100">
-      <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-    </a>
-  </div>
-  <div class="col-lg-3 col-md-4 col-xs-6">
-    <a href="#" class="d-block mb-4 h-100">
-      <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-    </a>
-  </div>
-  <div class="col-lg-3 col-md-4 col-xs-6">
-    <a href="#" class="d-block mb-4 h-100">
-      <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-    </a>
-  </div>
-  <div class="col-lg-3 col-md-4 col-xs-6">
-    <a href="#" class="d-block mb-4 h-100">
-      <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-    </a>
-  </div>
-  <div class="col-lg-3 col-md-4 col-xs-6">
-    <a href="#" class="d-block mb-4 h-100">
-      <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-    </a>
-  </div>
-  <div class="col-lg-3 col-md-4 col-xs-6">
-    <a href="#" class="d-block mb-4 h-100">
-      <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-    </a>
-  </div>
-  <div class="col-lg-3 col-md-4 col-xs-6">
-    <a href="#" class="d-block mb-4 h-100">
-      <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-    </a>
-  </div>
-  <div class="col-lg-3 col-md-4 col-xs-6">
-    <a href="#" class="d-block mb-4 h-100">
-      <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-    </a>
-  </div>
-  <div class="col-lg-3 col-md-4 col-xs-6">
-    <a href="#" class="d-block mb-4 h-100">
-      <img class="img-fluid img-thumbnail" src="http://placehold.it/400x300" alt="">
-    </a>
-  </div>
+@if (count($albums) > 0)
+
+  @foreach ($albums as $album)
+    <div class="col-lg-3 col-md-4 col-xs-6">
+    <a href="{{url('/home/albums',$album->id)}}" class="d-block mb-4  h-100">
+        <img class="img-fluid img-thumbnail" src="storage/album_covers/{{$album->cover_image}}" alt="Gallery {{$album->album_name}}">
+      </a>
+    </div>
+  @endforeach
+
+@else
+    <p>No Album To Display</p>
+@endif
+
+
 </div>
 
 @include ('inc.pagination')
